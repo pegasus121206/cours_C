c608b2d7875858d66aa250af967157f1fddb71a4
pegasus121206 <tiavina121206@gmail.com>
2023-08-16 16:11:33 +0300

    sa

--- string.c
+++ string.c
@@ -4,11 +4,11 @@
 
 int main(void)
 {
-    string s = get_string("Input: ")
+    string s = get_string("Input: ");
     printf("Output: ");
-    for (int i = 0; i < strlen(s); i++ );
+    for (int i = 0; i < strlen(s); i++ )
     {
-        printf("%c", i[s]);
+        printf("%c", s[i]);
     }
-    printf("\n")
+    printf("\n");
 }
