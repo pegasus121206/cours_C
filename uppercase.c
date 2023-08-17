9caba2c94be5f1da437830bfaf02a8907e24b805
pegasus121206 <tiavina121206@gmail.com>
2023-08-17 16:21:22 +0300

    er

--- uppercase.c
+++ uppercase.c
@@ -1,4 +1,5 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -8,7 +9,7 @@ int main(void)
     printf("After: ");
     for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if (s[i] >= 'a' && s[i] <= 'z')
+        if (islower(s[i]))
         {
             printf("%c", s[i] - 32);
         }
